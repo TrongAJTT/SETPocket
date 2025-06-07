0d61660bafaeb79c92e7f1a4973424b4f3ae6a34
TrongAJTT <157729907+TrongAJTT@users.noreply.github.com>
2025-06-07 15:04:55 +0700

    Hotfix to build Android release.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,22 +6,22 @@ plugins {
 }
 
 android {
-    namespace = "com.example.my_multi_tools"
+    namespace = "com.multitools.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_multi_tools"
+        // Application ID for Multi Tools app
+        applicationId = "com.multitools.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/multitools/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/multitools/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.my_multi_tools
+package com.multitools.app
 
 import io.flutter.embedding.android.FlutterActivity
 
